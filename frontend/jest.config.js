27bb9815c600084284a8918ba3897a7c11ae2af9
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-21 20:30:03 -0500

    Configure jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,7 @@
+import { createDefaultPreset } from 'ts-jest';
+
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'node',
+  transform: createDefaultPreset().transform,
+};
