d326e99e1dc6fe0a5d7c36d40abdd9eac8d9ad19
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-22 14:35:35 -0500

    Add tailwind

--- frontend-redo/src/App.tsx
+++ frontend-redo/src/App.tsx
@@ -3,7 +3,7 @@ import { PhaserHost } from './ui/phaser/PhaserHost';
 export default function App() {
   return (
     <>
-      <h1>Haunted House Tycoon</h1>
+      <h1 className="text-3xl font-bold">Haunted House Tycoon</h1>
       <PhaserHost />
     </>
   );
